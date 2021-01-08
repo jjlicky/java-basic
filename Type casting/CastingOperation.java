08fd9c6b1b3cf7d975f0fe925286a103b71cc815
jjlicky <jjlicky@yonsei.ac.kr>
2021-01-08 15:45:41 +0900

    data type

---
+++ Type casting/CastingOperation.java
@@ -0,0 +1,15 @@
+class CastingOperation
+{
+	public static void main(String[] args)
+	{
+		char ch1 = 'A';
+		char ch2 = 'J';
+
+		int num1=ch1;
+		int num2=(int)ch2;
+
+		System.out.println("문자 A의 유니코드 값 : "+num1);
+		System.out.println("문자 J의 유니코드 값 : "+num2);
+	
+	}
+}
